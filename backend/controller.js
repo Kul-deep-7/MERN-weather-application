f316fd8566c3e47ca3e33ffd21800cd59f0e6548
Kul-deep-7 <sahiljangid321@gmail.com>
2026-03-20 21:56:08 +0530

    backend controller

---
+++ backend/controller.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+
+const getAPIData = async function (req, res) {
+try {
+        const {city,} = req.body
+    
+        if(!city){
+            return res.status(400).json({ message: "City is required" })
+        }
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.APIkey}`)
+        const API_data = response.data
+
+
+        return res
+        .status(200)
+        .json(
+            {
+                API_data
+            }
+        ) 
+} catch (error) {
+    return res
+    .status(500)
+    .json({
+        message: error.message
+    })
+    
+}
+}
