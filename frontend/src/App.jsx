6d95f7ea321aaac6d210c49435304f1111d2e87c
Kul-deep-7 <sahiljangid321@gmail.com>
2026-03-24 21:16:24 +0530

    frontend logic done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,27 @@ import { useState } from 'react'
 import axios from 'axios'
 
 function App() {
-  const [weather,getweather ] = useState('')
+  const [city,setCity ] = useState('')
   const [loading, setLoading] = useState(false)
-  const [get, setget] = useState('')
+  const [temperature, setTemperature] = useState('')
 
 
   const handleSubmit = async()=>{
+    if (!city) {
+      alert("Enter a city")
+      return
+    }
+    setTemperature('')
+    setLoading(true)
   
     try {
-      const res = await axios.get(`http://localhost:5000/api/weather?city=${weather}`)
-      setget(res.data)
+      const res = await axios.get(`http://localhost:5000/api/weather?city=${city}`)
+      const tempK = res.data.API_data.main.temp
+      const tempC = ( tempK - 273.15).toFixed(2)
+      setTemperature(tempC)
       console.log(res.data)
     } catch (error) {
-       console.error("Error fetching comments:", error)
+       console.error("Error fetching dataoka:", error)
     } finally {
       setLoading(false)
     }
@@ -24,9 +32,28 @@ function App() {
     <>
     <h1>Weather APP</h1>
     <div>
-      <input type='text' value={weather} onChange={(e)=>getweather(e.target.value)}/>
+      <input type='text'
+      placeholder='Enter city name...' 
+      value={city} 
+      onChange={(e)=>setCity(e.target.value)}/>
       <button onClick={handleSubmit}>Search</button>
     </div>
+
+    {loading ? (<p>Loading...</p> 
+    ):( 
+      <>
+      <h1>City : {city}</h1>
+      {temperature ? (
+        <h3>{temperature}°celsius</h3>
+        )   :  loading ? 
+        (<p>Loading...</p>
+        )   :   
+        (`no data available for ${city}`
+      )}
+      </>
+    )}
+    
+
     </>
   )
 }
